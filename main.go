353f274a2029c0a4062c469881acbb0650cbb58b
nestera11 <nesterov@integra-s.com>
2025-05-27 12:49:04 +0400

    +

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Knetic/govaluate"
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 type Calculation struct {
 	Id         string `json:"id"`
@@ -14,6 +22,49 @@ type CalculationRequest struct {
 
 var calculations = []Calculation{}
 
+func calcExpr(expression string) (string, error) {
+	expr, err := govaluate.NewEvaluableExpression(expression)
+	if err != nil {
+		return "", err
+	}
+	res, err := expr.Evaluate(nil)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%v", res), nil
+}
+
+func getCalcs(c echo.Context) error {
+	return c.JSON(http.StatusOK, calculations)
+}
+
+func postCalc(c echo.Context) error {
+	var req CalculationRequest
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
+	}
+	res, err := calcExpr(req.Expression)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid expression"})
+	}
+	calc := Calculation{
+		Id:         uuid.NewString(),
+		Expression: req.Expression,
+		Result:     res,
+	}
+	calculations = append(calculations, calc)
+	return c.JSON(http.StatusCreated, calc)
+}
+
 func main() {
-	fmt.Println("hello")
+
+	e := echo.New()
+	e.Use(middleware.CORS())
+	e.Use(middleware.Logger())
+
+	e.GET("/calculations", getCalcs)
+	e.POST("/calculations", postCalc)
+
+	e.Start("localhost:8080")
+
 }
