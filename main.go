0fa6658ed56b2e61b2b0f03bdc900181496d06d6
nestera11 <bjik0chik@gmail.com>
2025-05-28 10:53:37 +0400

    +

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 
 type Calculation struct {
 	Id         string `json:"id"`
-	Expression string `json:"expretion"`
+	Expression string `json:"expression"`
 	Result     string `json:"result"`
 }
 
@@ -56,6 +56,39 @@ func postCalc(c echo.Context) error {
 	return c.JSON(http.StatusCreated, calc)
 }
 
+func patchCalc(c echo.Context) error {
+	id := c.Param("id")
+	var req CalculationRequest
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
+	}
+	res, err := calcExpr(req.Expression)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid expression"})
+	}
+
+	for i, v := range calculations {
+		if v.Id == id {
+			calculations[i].Expression = req.Expression
+			calculations[i].Result = res
+			return c.JSON(http.StatusOK, calculations[i])
+		}
+	}
+	return c.JSON(http.StatusBadRequest, map[string]string{"error": "Calcilation not found"})
+}
+
+func deleteCalc(c echo.Context) error {
+	id := c.Param("id")
+	for i, v := range calculations {
+		if v.Id == id {
+			calculations = append(calculations[:i], calculations[i+1:]...)
+			return c.NoContent(http.StatusNoContent)
+		}
+	}
+	return c.JSON(http.StatusBadRequest, map[string]string{"error": "Calcilation not found"})
+
+}
+
 func main() {
 	fmt.Println("------------------")
 	e := echo.New()
@@ -64,6 +97,8 @@ func main() {
 
 	e.GET("/calculations", getCalcs)
 	e.POST("/calculations", postCalc)
+	e.PATCH("/calculations/:id", patchCalc)
+	e.DELETE("/calculations/:id", deleteCalc)
 
 	e.Start("localhost:8080")
 
